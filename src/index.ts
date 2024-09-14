b8e255ff7e383d1c1e4ba547096e820ea136f486
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-09-14 10:33:13 +0200

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { rateLimit } from 'express-rate-limit'
+import path from 'path'
 import 'dotenv/config'
 
 import TodosRouter from './router/todos'
@@ -14,6 +15,7 @@ const limiter = rateLimit({
 
 app.use(limiter)
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
